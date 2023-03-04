64dec613d6325dc5f5e4e17d74e740899246e3a4
Burak Özlece <burakozleceplus@gmail.com>
2023-03-04 16:24:16 +0300

    Entity katmanı dolduruldu

---
+++ Traversal.Entity/Concrete/About2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traversal.Entity.Concrete
+{
+    public class About2
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Decription { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ Traversal.Entity/Concrete/Guides.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traversal.Entity.Concrete
+{
+    public class Guides
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Decription { get; set; }
+        public string Image { get; set; }
+        public string TwitterUrl { get; set; }
+        public string InstagramUrl { get; set; }
+        public bool Status  { get; set; }
+    }
+}
